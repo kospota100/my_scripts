d93a728c7496825cf23f4ab635c8bb8ff9f1067d
kospota100 <kamigod0907@gmail.com>
2023-03-11 00:29:59 +0800

    Updated local2remote.sh - 新增指定 SSH Host port 的功能

--- Linux/local2remote.sh
+++ Linux/local2remote.sh
@@ -4,7 +4,7 @@ if [ $1 = "help" ]; then
 
 	echo "\n\t======= local2remote use manual =======\n"
 	echo "  Usage:"
-	echo "    sh local2remote.sh [Option] [File/folder] [Remote username] [Remote IP addr]\n"
+	echo "    sh local2remote.sh [Option] [File/folder] [Remote username] [Remote IP addr] [Remote port]\n"
 	echo "  Option:"
         echo "    help  show usage manual"
         echo "    -e    execute"
@@ -13,7 +13,7 @@ if [ $1 = "help" ]; then
 
 elif [ $1 = "-e" ]; then
 
-	echo "\nCopy local file $2 to remote machine $3@$4:~/remote folder\n"
+	echo "\nCopy local file $2 to remote machine $3@$4 port $5:~/remote folder\n"
 
-	rsync -avzh -e 'ssh -p 8022' --progress ./$2 $3@$4:~/remote/
+	rsync -avzh -e "ssh -p ${5}" --progress ./$2 $3@$4:~/remote/
 fi
